f91d76cfd8c6729582e7864fcd3c8d8fbe079787
Donnie Ferris <109878120+don-ferris@users.noreply.github.com>
2025-11-04 14:14:12 -0500

    Create backrest.py for interactive backup tool
    Initial implementation of BackRest, an interactive backup/restore tool.

---
+++ backrest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+: <<'SCRIPT_HEADER'
+backrest.sh
+BackRest - interactive backup/restore tool (front-end for partclone + dd). Fully self-contained on bootable drive - backs up to/restores from /imgstore on same drive.
+──────────────────────────────────────────────────────
+Author: Don Ferris
+Created: 2025-11-04
+Current Revision: 1.0
+──────────────────────────────────────────────────────
+Revision History
+================
+v1.0 — 2025-10-24 — Initial implementation: A front-end for dd, BackRest displays a list of drives/partitions (for backup) or image files (for restore) as a menu with one-key menu item selecto[...]
+──────────────────────────────────────────────────────
+
+# END OF
+SCRIPT_HEADER
